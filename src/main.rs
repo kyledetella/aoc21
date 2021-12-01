ab0bee77f45acf0a7fc8a48a283d21f88d00b98a
Kyle DeTella <kdetella@gmail.com>
2021-12-01 11:00:34 -0600

    Day 1 - part 2

--- src/main.rs
+++ src/main.rs
@@ -12,39 +12,37 @@ where
 
 fn main() {
     if let Ok(input) = read_input("day1.txt") {
-        let mut depth_changes = 0;
-        let mut last_depth: i32 = 0;
+        let mut depths: Vec<i32> = Vec::new();
 
-        for (idx, line) in input.enumerate() {
+        // First pass
+        for line in input {
             if let Ok(ln) = line {
-                if idx > 0 {
-                    let depth = ln.parse::<i32>().unwrap();
-
-                    if depth > last_depth {
-                        depth_changes += 1;
-                    }
-
-                    last_depth = depth;
-                }
+                depths.push(ln.parse::<i32>().unwrap());
             }
         }
 
-        println!("Depth changes {}", depth_changes);
+        let mut windows: Vec<i32> = Vec::new();
+
+        for (idx, depth) in depths.iter().enumerate() {
+            if idx < depths.len() - 2 {
+                windows.push(depth.to_owned() + depths[idx + 1] + depths[idx + 2]);
+                // println!("[{}, {}, {}]", depth, depths[idx + 1], depths[idx + 2]);
+            }
+        }
+
+        let mut depth_changes = 0;
+        let mut last_depth: &i32 = &0.to_owned();
+
+        for window in windows.iter() {
+            println!("{}, {}", window, last_depth);
+
+            if window > last_depth {
+                depth_changes += 1;
+            }
+
+            last_depth = window;
+        }
+
+        println!("Depth changes {}", depth_changes - 1);
     }
 }
-
-// fn main() {
-//     let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-
-//     let mut depth_changes = 0;
-
-//     for (idx, depth) in depths.iter().enumerate() {
-//         if idx > 0 {
-//             if depth > &depths[idx - 1] {
-//                 depth_changes += 1;
-//             }
-//         }
-//     }
-
-//     println!("Depth changes: {}", depth_changes);
-// }
